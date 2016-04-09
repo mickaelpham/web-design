b715275e4e4dab6e33c4b783dad51b60e4399598
Mickael Pham <mickael.pham@gmail.com>
2016-04-08 19:45:21 -0700

    Add browser sync and various plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,61 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
+var autoprefixer = require('autoprefixer');
+var browserSync  = require('browser-sync');
+var cssnext      = require('postcss-cssnext');
+var del          = require('del');
+var gulp         = require('gulp');
+var imagemin     = require('gulp-imagemin');
+var postcss      = require('gulp-postcss');
+var precss       = require('precss');
+var sourcemaps   = require('gulp-sourcemaps');
 
-// Static server
-gulp.task('browser-sync', function() {
-  browserSync.init({
-    server: {
-      baseDir: './app'
-    }
-  });
+var paths = {
+  css:    'app/css/*.css',
+  html:   'app/*.html',
+  images: 'app/images/**/*'
+};
 
-  gulp.watch('app/*.html').on('change', browserSync.reload);
-  gulp.watch('app/css/*.css').on('change', browserSync.reload);
+// Clean up build folder
+gulp.task('clean', function() {
+  return del(['build']);
 });
 
-gulp.task('default', ['browser-sync']);
+// Post CSS transformation
+gulp.task('css', function() {
+  return gulp.src(paths.css)
+    .pipe(sourcemaps.init())
+    .pipe(postcss([autoprefixer, precss]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build/css'))
+    .pipe(browserSync.stream());
+});
+
+// Copy all static images
+gulp.task('images', function() {
+  return gulp.src(paths.images)
+    .pipe(imagemin({ optimizationLevel: 5 }))
+    .pipe(gulp.dest('build/images'));
+});
+
+// Copy all HTML files
+gulp.task('html', function() {
+  return gulp.src(paths.html)
+    .pipe(gulp.dest('build'));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch(paths.css, ['css']); // note: CSS are injected directly
+  gulp.watch(paths.html, ['html']).on('change', browserSync.reload);
+  gulp.watch(paths.images, ['images']).on('change', browserSync.reload);
+});
+
+// Static server
+gulp.task('browser-sync', ['watch'], function() {
+  browserSync.init({
+    server: {
+      baseDir: './build'
+    }
+  });
+});
+
+gulp.task('default', ['html', 'css', 'images', 'browser-sync']);
